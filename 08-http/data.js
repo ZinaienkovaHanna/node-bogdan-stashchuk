923c86afca9f04374f3ced1ded9e8188d402cd25
Hanna <zinaienkova.anna@gmail.com>
2023-05-27 07:01:26 +0300

    http

---
+++ 08-http/data.js
@@ -0,0 +1,19 @@
+const comments = [
+    {
+        id: 100,
+        text: 'First comment',
+        author: 'Bogdan',
+    },
+    {
+        id: 123,
+        text: 'Second comment',
+        author: 'Alice',
+    },
+    {
+        id: 146,
+        text: 'Last comment',
+        author: 'Bob',
+    },
+];
+
+module.exports = comments;
---
+++ 08-http/index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const {
+    getHome,
+    getHTML,
+    getText,
+    getComments,
+    handleNotFound,
+    postComment,
+} = require('./handlers.js');
+
+const PORT = 5000;
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        return getHome(req, res);
+    }
+
+    if (req.method === 'GET' && req.url === '/html') {
+        return getHTML(req, res);
+    }
+    if (req.method === 'GET' && req.url === '/text') {
+        return getText(req, res);
+    }
+    if (req.method === 'GET' && req.url === '/comments') {
+        return getComments(req, res);
+    }
+    if (req.method === 'POST' && req.url === '/comments') {
+        return postComment(req, res);
+    }
+    res.statusCode = 404;
+    return handleNotFound(req, res);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server was launched on port ${PORT}`);
+});
