3267de86f29274f7d0f1dad562884d79c2d9c079
ZinaienkovaHanna <zinaienkova.anna@gmail.com>
2023-06-25 15:05:41 +0300

    create stdin-pipe-stdout.mjs

--- 09-streams/stdin-pipe-stdout.mjs
+++ 09-streams/stdin-pipe-stdout.mjs
@@ -1,15 +1,22 @@
-import stream from 'stream';
+import { Transform } from 'stream';
 import fs from 'fs';
 
-const upperCaseStream = new stream.Transform({
+const upperCaseStream = new Transform({
     transform: function (chunk, encoding, cb) {
         const upperCased = chunk.toString().toUpperCase();
-        console.log(upperCased);
         cb(null, upperCased);
     },
 });
 
-process.stdin.pipe(upperCaseStream).pipe(process.stdout);
+const reverseStream = new Transform({
+    transform(chunk, encoding, cb) {
+        const arrayOfChars = chunk.toString().split('');
+        const lastChar = arrayOfChars.pop();
+        const reversed = arrayOfChars.reverse().concat(lastChar).join('');
+        cb(null, reversed);
+    },
+});
+process.stdin.pipe(upperCaseStream).pipe(reverseStream).pipe(process.stdout);
 
 // // Pipe to file
 // const filePath = './files/stdin-dump.txt';
